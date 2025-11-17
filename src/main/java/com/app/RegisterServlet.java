65304f60916856cd007b62ba32740967aec36e67
rohanrode02 <roderohan2@gmail.com>
2025-11-17 23:11:56 +0530

    all code

---
+++ src/main/java/com/app/RegisterServlet.java
@@ -0,0 +1,23 @@
+package com.app;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String course = req.getParameter("course");
+
+        StudentDAO dao = new StudentDAO();
+        dao.saveStudent(name, email, course);
+
+        resp.getWriter().println("Student Registered Successfully!");
+    }
+}
